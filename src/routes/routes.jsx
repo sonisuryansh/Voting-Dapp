fda6a4acfbfdc1183dad7ef34ee243322a8d397c
Suryansh Soni <146843858+sonisuryansh@users.noreply.github.com>
2026-02-08 11:16:06 +0530

    Navigation

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -11,12 +11,43 @@ import RegisterVoter from "../pages/Voter/RegisterVoter";
 import ElectionCommision from "../pages/ElectionCommision/ElectionCommision";
 import App from "../App";
 import Wallet from "../components/Wallet/Wallet.jsx";
+import Navigation from "../components/Navigation/Navigation.jsx";
 
 export const routes = createBrowserRouter([
-    {path:'/',element:<Wallet/>},
-    {path:'register-voter', element:<RegisterVoter></RegisterVoter>},
-    {path:'register-candidate', element:<RegisterCandidate></RegisterCandidate>},
-    {path:'voter-list', element:<GetVoterList></GetVoterList>},
-    {path:'candidate-list', element:<GetCandidateList></GetCandidateList>},
-    {path:'election-commision', element:<ElectionCommision></ElectionCommision>}
+    {path:'/',element:(
+    <div>
+        <Navigation></Navigation>
+        <Wallet/>
+    </div>
+    )},
+    {path:'register-voter', element:(
+    <div>
+        <Navigation></Navigation>
+        <RegisterVoter/>
+    </div>
+    )},
+    {path:'register-candidate', element:(
+    <div>
+        <Navigation></Navigation>
+        <RegisterCandidate/>
+    </div>
+    )},
+    {path:'voter-list', element:(
+    <div>
+        <Navigation></Navigation>
+        <GetVoterList/>
+    </div>
+    )},
+    {path:'candidate-list', element:(
+    <div>
+        <Navigation></Navigation>
+        <GetCandidateList/>
+    </div>
+    )},
+    {path:'election-commision', element:(
+    <div>
+        <Navigation></Navigation>
+        <ElectionCommision/>
+    </div>
+    )}
 ]);
